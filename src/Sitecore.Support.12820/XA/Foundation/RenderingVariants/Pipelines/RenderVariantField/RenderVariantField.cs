219d8dbad08901042a635f3c3ef428002e117eaf
DK\NPA <npa@sitecore.net>
2018-05-16 15:23:04 +0300

    Native code

---
+++ src/Sitecore.Support.12820/XA/Foundation/RenderingVariants/Pipelines/RenderVariantField/RenderVariantField.cs
@@ -0,0 +1,50 @@
+﻿using Sitecore.XA.Foundation.RenderingVariants.Fields;
+using Sitecore.XA.Foundation.Variants.Abstractions.Pipelines.RenderVariantField;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+
+namespace Sitecore.Support.XA.Foundation.RenderingVariants.Pipelines.RenderVariantField
+{
+  public class RenderVariantField: Sitecore.XA.Foundation.RenderingVariants.Pipelines.RenderVariantField.RenderVariantField
+  {
+    protected override Control Render(VariantField variantField, RenderVariantFieldArgs args)
+    {
+      Control variantFieldNameLiteral;
+      if (string.IsNullOrEmpty(variantField.FieldName))
+      {
+        return new LiteralControl();
+      }
+      if (this.IsEmptyFieldToRender(variantField, args.Item) || this.IsFromSnippedAndEmpty(variantField, args.Item, args.IsControlEditable))
+      {
+        return new LiteralControl();
+      }
+      if (args.Item.Fields[variantField.FieldName] != null)
+      {
+        variantFieldNameLiteral = this.CreateFieldRenderer(variantField, args.Item, args.IsControlEditable, args.IsFromComposite);
+      }
+      else if (args.IsControlEditable && Context.PageMode.IsExperienceEditorEditing)
+      {
+        variantFieldNameLiteral = this.GetVariantFieldNameLiteral(variantField);
+      }
+      else
+      {
+        return new LiteralControl();
+      }
+      variantField.IsLink = this.ProtectLink(variantField.FieldName, variantField.IsLink, args.Item);
+      variantFieldNameLiteral = this.HandleAffixAndLinkCreation(variantFieldNameLiteral, args.Item, variantField.Prefix, variantField.Suffix, variantField.IsLink, variantField.IsDownloadLink, variantField.IsPrefixLink, variantField.IsSuffixLink, variantField.LinkAttributes, variantField.LinkField, args.HrefOverrideFunc);
+      if (!string.IsNullOrWhiteSpace(variantField.Tag))
+      {
+        HtmlGenericControl tag = new HtmlGenericControl(variantField.Tag);
+        this.AddClass(tag, $"{variantField.CssClass} {this.GetFieldCssClass(variantField.FieldName)}".Trim());
+        this.AddWrapperDataAttributes(variantField, args, tag);
+        this.MoveControl(variantFieldNameLiteral, tag);
+        variantFieldNameLiteral = tag;
+      }
+      return variantFieldNameLiteral;
+    }
+  }
+}
